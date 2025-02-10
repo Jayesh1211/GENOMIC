769495ba54b9ae6ea32fcdb8c4e71766b9f342f8
Jayesh  Hire <114867688+Jayesh1211@users.noreply.github.com>
2025-02-10 15:24:51 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -397,7 +397,7 @@ else:
     st.warning("Please upload a preprocessed CSV file to begin training.")
 
 st.markdown("### Details  \n"
-            "**Number of Clients**: Defines how many clients participate in the Federated Learning process, each contributing to model training.  \n"
-            "**Number of Epochs**: Specifies how many times the entire dataset is processed by each client during training.  \n"
-            "**Max Training Iterations**: Sets the upper limit on the number of optimization steps each client performs per training round.  \n"
-            "**Samples per Epoch**: Determines how many data samples each client processes in a single epoch.")
+           - "**Number of Clients**: Defines how many clients participate in the Federated Learning process, each contributing to model training.  \n"
+           - "**Number of Epochs**: Specifies how many times the entire dataset is processed by each client during training.  \n"
+           - "**Max Training Iterations**: Sets the upper limit on the number of optimization steps each client performs per training round.  \n"
+           - "**Samples per Epoch**: Determines how many data samples each client processes in a single epoch.")
